c4c1c071a0bdecdb026c1a9726313a7d16e3e4a4
raoni.rodriguess@gmail.com <raoni.rodriguess@gmail.com>
2022-02-14 01:13:27 -0300

    Finalizando classe de dominio Categoria

--- src/main/java/com/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.cursomc.cursomc.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.cursomc.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Funcionou aqui";
+
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
