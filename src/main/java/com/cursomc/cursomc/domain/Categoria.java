8cc2a87b892b833025f1596d2c4204b66e40586c
raoni.rodriguess@gmail.com <raoni.rodriguess@gmail.com>
2022-02-14 21:55:58 -0300

    Criando repository e service para Categoria

--- src/main/java/com/cursomc/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/cursomc/domain/Categoria.java
@@ -1,11 +1,19 @@
 package com.cursomc.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1l;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
@@ -34,27 +42,15 @@ public class Categoria implements Serializable {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Categoria other = (Categoria) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
---
+++ src/main/java/com/cursomc/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.cursomc.cursomc.services;
+
+import com.cursomc.cursomc.domain.Categoria;
+import com.cursomc.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
