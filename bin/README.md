0465a76b82eb21955de7a6fe06efcbf4c65b57be
raoni.rodriguess@gmail.com <raoni.rodriguess@gmail.com>
2022-02-18 00:59:13 -0300

    ajuste no endpoint /categorias/{id} incluindo mensagens de erro em caso de objetos não encontrados

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# Training-Spring-Boot
+Full stack project complete! Java Spring Boot on the back end and Ionic on the front end. From UML design to Heroku and Play Store!
--- src/main/java/com/cursomc/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.cursomc.cursomc.services;
 
 import com.cursomc.cursomc.domain.Categoria;
 import com.cursomc.cursomc.repositories.CategoriaRepository;
+import com.cursomc.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
+
 }
